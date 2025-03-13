51972499a7d99514eaba1d29367a51ff3ac8a03e
Hatta <hattagithub@gmail.com>
2025-03-14 01:06:07 +0700

    feat: add api documentation for login and register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -64,6 +64,12 @@ export default {
   },
 
   async login(req: Request, res: Response) {
+    /**
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: "#/components/schemas/LoginRequest"}
+     }
+     */
     const { identifier, password } = req.body as unknown as TLogin;
     try {
       // Ambil data user berdasarkan "identifier" => email dan username
@@ -116,6 +122,11 @@ export default {
   },
 
   async me(req: IReqUser, res: Response) {
+    /**
+     #swagger.security = [{
+      "bearerAuth": []
+     }]
+     */
     try {
       const user = req.user;
       const result = await UserModel.findById(user?.id);
---
+++ src/docs/swagger.ts
@@ -0,0 +1,39 @@
+import { version } from "mongoose";
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API ACARA",
+    description: "Dokumentasi API ACARA",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Server",
+    },
+    {
+      url: "https://back-end-acara-roan.vercel.app/",
+      description: "Deploy Server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "hattayudia",
+        password: "12345",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
